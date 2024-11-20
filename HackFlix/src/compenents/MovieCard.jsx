615f8ad54b8884ddf62e51979670cdddc8f3a006
Tomas <tom3432w@gmail.com>
2024-11-19 23:06:54 -0300

    hoover

--- HackFlix/src/compenents/MovieCard.jsx
+++ HackFlix/src/compenents/MovieCard.jsx
@@ -1,15 +1,25 @@
-// src/components/MovieCard.jsx
+
 import React from "react";
 import "./MovieCard.css";
 
 const MovieCard = ({ movie, columns }) => {
   return (
     <div className={`col-${columns} mt-3`}>
-      <img
-        className="img-fluid"
-        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-        alt={movie.title}
-      />
+      <div className="movie-poster">
+        <img
+          className="img-fluid"
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt={movie.title}
+        />
+        <div className="movie-info">
+          <h3>{movie.title}</h3>
+          <p>
+            {movie.overview
+              ? movie.overview.slice(0, 100) + "..."
+              : "No description available"}
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
