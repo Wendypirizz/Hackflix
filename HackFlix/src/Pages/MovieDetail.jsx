22417d6491bcb20a33cf836a77929464302feaae
Tomas <tom3432w@gmail.com>
2024-11-26 21:36:13 -0300

    MovieDetail style

--- HackFlix/src/Pages/MovieDetail.jsx
+++ HackFlix/src/Pages/MovieDetail.jsx
@@ -14,20 +14,33 @@ function MovieDetail({ movieList }) {
     : "path_to_default_image.jpg";
 
   return (
-    <div className="movie-detail-container">
-      <h1>{movie.title}</h1>
-      <img src={posterSrc} alt={movie.title} className="movie-detail-poster" />
-      <p>
-        <strong>Descripción:</strong>{" "}
-        {movie.overview || "No description available"}
-      </p>
-      <p>
-        <strong>Fecha de estreno:</strong> {movie.release_date}
-      </p>
-      <p>
-        <strong>Calificación:</strong> {movie.vote_average}
-      </p>
+    <div className="container-fluid py-5" style={{ 
+      background: "linear-gradient(to right, rgba(0, 0, 0, 0.7), rgba(34, 34, 34, 0.7))", 
+      borderRadius: "15px",
+      color: "#fff" 
+    }}>
+  <div className="row align-items-center">
+    
+    <div className="col-md-3 d-flex justify-content-center">
+      <img 
+        src={posterSrc} 
+        alt={movie.title} 
+        className="img-fluid rounded shadow" 
+        style={{ maxWidth: "100%", height: "auto", maxHeight: "300px" }} 
+      />
     </div>
+    
+    <div className="col-md-9">
+      <div className="text-center mb-4">
+        <h1 className="fw-bold">{movie.title}</h1>
+      </div>
+      <p><strong>Descripción:</strong> {movie.overview || "No description available"}</p>
+      <p><strong>Fecha de estreno:</strong> {movie.release_date}</p>
+      <p><strong>Calificación:</strong> {movie.vote_average}</p>
+    </div>
+  </div>
+</div>
+
   );
 }
 
