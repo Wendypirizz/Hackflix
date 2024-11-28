2a7bb9666a49280cf2219390b248592c91ea6223
socram11 <markilopez4@gmail.com>
2024-11-27 22:12:36 -0300

    test for github

--- HackFlix/src/compenents/ReviewCard.jsx
+++ HackFlix/src/compenents/ReviewCard.jsx
@@ -4,15 +4,19 @@ const ReviewCard = ({ review }) => {
   return (
     <div className="row align-items-center p-3 bg-black m-3 rounded ">
       <div className="col-3">
-        <img className="img-fluid rounded" src={review.poster_path} alt="" />
+        <img
+          className="img-fluid rounded"
+          src={`https://image.tmdb.org/t/p/w500${review.poster_path}`}
+          alt=""
+        />
       </div>
       <div className="col-9 text-start position-relative object-fit-cover">
         <div className="position-absolute top-0 start-0 object-fit-cover">
           <div className="">
-            <h2 className="">{review.title}</h2>
+            <h2 className="">{review.tema}</h2>
           </div>
           <div>
-            <p>{review.overview}</p>
+            <p>{review.contenido}</p>
           </div>
         </div>
       </div>
